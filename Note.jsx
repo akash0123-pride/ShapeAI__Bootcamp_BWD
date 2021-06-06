63a75224401443eee18a67729473bf53e78e5fa6
akash0123-pride <85451287+akash0123-pride@users.noreply.github.com>
2021-06-06 22:32:42 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note(){
+  return(
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>This was amazing bootcamp
+        taken up by Shaurya sir 
+        we covered everything 
+        from scratch including 
+        javascript,React js,Html
+       </p>
+     </div>  
+  );
+}
+export default Note;
